4f85e2fb477ae79b129f03391a0cd705eb0948c4
eriksmith2024 <eriksmith.career@hotmail.com>
2024-09-16 14:42:53 +0100

    update

--- headfirst JavaScript/C2/battleship.js
+++ headfirst JavaScript/C2/battleship.js
@@ -2,8 +2,13 @@ var location1 = 3;
 var location2 = 4;
 var location3 = 5;
 
-var guess = 0;
+var guess;
 var hits = 0;
 var guesses = 0;
 
 var isSunk = false;
+
+
+while (isSunk == false){
+    guess = prompt ("Ready, aim, fire! (enter a number from 0-6):");
+}
