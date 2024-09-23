6923d6838c6f472e57830032efec525ce161dafd
eriksmith2024 <eriksmith.career@hotmail.com>
2024-09-23 15:22:22 +0100

    update

--- C8/battleship.js
+++ C8/battleship.js
@@ -1,4 +1,11 @@
 
 var view = {
-    displaymessage
-}
+    displaymessage: function(msg) {
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg
+    },
+    displayHit: function(location) {
+    },
+    displayMiss: function(location) {
+    }
+};
