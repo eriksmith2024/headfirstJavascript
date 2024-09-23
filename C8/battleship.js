ad996d16e822952bfa09ad9520d96113354601ce
eriksmith2024 <eriksmith.career@hotmail.com>
2024-09-23 16:06:30 +0100

    updates

--- C8/battleship.js
+++ C8/battleship.js
@@ -1,11 +1,34 @@
+var model = {
+    boardSize:7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+
+    ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+            { locations: ["24", "34", "44"], hits: ["", "", ""] },
+            { locations: ["10", "11", "12"], hits: ["", "", ""] }]
+};
 
 var view = {
     displaymessage: function(msg) {
         var messageArea = document.getElementById("messageArea");
-        messageArea.innerHTML = msg
+        messageArea.innerHTML = msg;
     },
     displayHit: function(location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", hit);
     },
     displayMiss: function(location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
     }
 };
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+view.displaymessage("Tap tap us this thing on");
